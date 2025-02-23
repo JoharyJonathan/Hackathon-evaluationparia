6a39a1bd4ce341c337c396951ce7ad823fd54ac6
IhantsaFana <ihantsarakotondranaivo@gmail.com>
2025-02-23 15:49:59 -0800

    Commit avy aty amin'ny front 1

--- client/app/(auth)/dashboard/student/page.tsx
+++ client/app/(auth)/dashboard/student/page.tsx
@@ -1,8 +1,16 @@
+import Card from "@/app/components/ui/Card";
+
 export default function StudentDashboard() {
     return (
       <div>
         <h1 className="text-3xl font-bold mb-4">Dashboard Étudiant</h1>
-        <p>Bienvenue sur votre espace étudiant.</p>
+        
+        <Card>
+          <h2 className="text-xl font-semibold">Faire un examen</h2>
+          <p className="text-gray-600">
+            Sélectionnez un examen parmi les examens disponibles ci-dessous.
+          </p>
+        </Card>
       </div>
     );
   }
