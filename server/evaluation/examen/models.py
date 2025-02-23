24dc1a80340d3379d77765993b00ea567c5b8bc3
JoharyJonathan <rjoharyjonathan@gmail.com>
2025-02-23 12:13:27 +0300

    Utilisation de model User personnalise

--- server/evaluation/examen/models.py
+++ server/evaluation/examen/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 class Exam(models.Model):
     title = models.CharField(max_length=255)
@@ -7,7 +7,48 @@ class Exam(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     deadline = models.DateTimeField()
     duration = models.IntegerField(help_text="Durée en minutes")
-    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="exams")
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="exams")
 
     def __str__(self):
         return self.title
+
+class Question(models.Model):
+    QUESTION_TYPES = [
+        ('QCM', 'QCM'),
+        ('courte', 'Courte'),
+        ('longue', 'Longue'),
+        ('code', 'Code'),
+    ]
+    
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE, related_name="questions")
+    question_text = models.TextField()
+    meta_data = models.JSONField(blank=True, null=True)
+    type = models.CharField(max_length=10, choices=QUESTION_TYPES, default='QCM')
+
+    def __str__(self):
+        return self.text
+    
+class Response(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="responses")
+    student = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="responses")
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.student.name} - {self.exam.title}"
+    
+class Answer(models.Model):
+    response = models.ForeignKey(Response, on_delete=models.CASCADE, related_name="answers")
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="answers")
+    answer_text = models.TextField()  
+    score = models.FloatField(blank=True, null=True)  
+
+    def __str__(self):
+        return f"Answer for {self.question.id}"
+    
+class Feedback(models.Model):
+    response = models.OneToOneField(Response, on_delete=models.CASCADE, related_name="feedback")
+    feedback_text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Feedback for {self.response.exam.title}"
