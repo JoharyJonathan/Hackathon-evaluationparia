c9ea7f9544f1062beb871fd2835f06c8c205b451
JoharyJonathan <rjoharyjonathan@gmail.com>
2025-02-23 09:07:33 +0300

    Ajout de python-dotenv

--- server/evaluation/evaluation/settings.py
+++ server/evaluation/evaluation/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'evaluation.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "hackeva",
+        "USER": "postgres",
+        "PASSWORD": "berserker",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
