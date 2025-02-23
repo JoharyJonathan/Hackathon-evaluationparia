e831b839c19fe32d0b8f5b1ebb85f65a250882a4
JoharyJonathan <rjoharyjonathan@gmail.com>
2025-02-23 13:22:47 +0300

    Actions du professeur

---
+++ server/evaluation/examen/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Exam, Question, Response
+
+class ExamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Exam
+        fields = '__all__'
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = '__all__'
+
+class ResponseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Response
+        fields = '__all__'
